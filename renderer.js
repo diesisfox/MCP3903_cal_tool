c022277fb0599f66177df040a82d5b4c2d42fda4
diesisfox <james.zhi.liu@gmail.com>
2017-06-12 12:49:47 -0400

    did a few more calibrations; added id buttons

--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@
 const $ = require('jquery');
 const SerialPort = require('serialport');
 const {clipboard} = require('electron')
-var port, continuousMode = false;
+var port, continuousMode = false, listenID = 0x201;
 // var pastReadings = [];
 
 // ##     ## ######## #### ##
@@ -38,6 +38,13 @@ window.onload = function(){
 	}
 	$('#copyBtn')[0].onclick = copyData;
 	$('#continuousBtn')[0].onclick = contBtnCb;
+	$('#idBtnGroup').children().click((e)=>{
+		listenID = parseInt($(e.target).text(), 16);
+		$('#idBtnGroup').children().addClass('btn-default');
+		$(e.target).removeClass('btn-default');
+	});
+
+	$('#id201btn').click();
 }
 
 function contBtnCb(){
@@ -156,7 +163,7 @@ function parseData(data){
 
 		if(frame.type == 'frame'){
 			frame.id = parseInt(frame.id, 16);
-			if((frame.id & 0xfff) == 0x203){
+			if((frame.id & 0xfff) == listenID){
 				$('#hex0')[0].innerText = frame.data[2] + frame.data[1] + frame.data[0];
 				$('#hex1')[0].innerText = frame.data[6] + frame.data[5] + frame.data[4];
 				for(let i=0; i<frame.data.length; i++){
@@ -172,13 +179,25 @@ function parseData(data){
 
 				$('#dec0')[0].innerText = reading[0];
 				$('#rat0')[0].innerText = reading[0] * 100 / 0x800000;
-				$('#vad0')[0].innerText = (reading[0] * 2.33) /(8388608 * 3 * 1);;
-				$('#vin0')[0].innerText = psb0ch0Map(reading[0]);
+				$('#vad0')[0].innerText = (reading[0] * 2.33) /(8388608 * 3 * 1);
 
 				$('#dec1')[0].innerText = reading[1];
 				$('#rat1')[0].innerText = reading[1] * 100 / 0x800000;
-				$('#vad1')[0].innerText = (reading[1] * 2.33) /(8388608 * 3 * 1);
-				$('#vin1')[0].innerText = psb0ch1Map(reading[1]);
+				$('#vad1')[0].innerText = (reading[1] * 2.33) /(8388608 * 3 * 2);
+
+				switch (listenID) {
+					case 0x201:
+						$('#vin0')[0].innerText = psb0ch0Map(reading[0]);
+						$('#vin1')[0].innerText = psb1ch1Map(reading[1]);
+						break;
+					case 0x202:
+					case 0x203:
+						$('#vin0')[0].innerText = psb0ch4Map(reading[0]);
+						$('#vin1')[0].innerText = psb0ch5Map(reading[1]);
+						break;
+					default:
+						break;
+				}
 
 				if(continuousMode) copyData();
 			}
@@ -194,12 +213,16 @@ function psb0ch1Map(raw){
 	return (raw + 30217.2042253521) / 163576.265422321;
 }
 
+function psb0ch4Map(raw){
+	return (raw+17051.8328322493)/33134.5112988709;
+}
+
 function psb0ch5Map(raw){
 	return (raw-12693.6707317073)/324921.553523035;
 }
 
-function psb0ch4Map(raw){
-	return (raw+17051.8328322493)/33134.5112988709;
+function psb1ch1Map(raw){
+	return (raw-69080.9705882353)/165597.927315084;
 }
 
 function vinViaInterpolaion(raw){
